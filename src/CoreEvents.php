40693244d66da8c6f5482ac90a0c4951047f9dec
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-05-21 16:32:45 +0400

    edited CookieInterface

--- src/CoreEvents.php
+++ src/CoreEvents.php
@@ -9,4 +9,7 @@ interface CoreEvents
     public const PARSE_ATTRIBUTES    = 'PARSE_ATTRIBUTES';
     public const SIMPLE_EVENTS       = 'SIMPLE_EVENTS';
     public const HTTP_REQUEST_EVENTS = 'HTTP_REQUEST_EVENTS';
+
+    public const ROUTE_MATCH_EVENTS = 'ROUTE_MATCH_EVENTS';
+    public const RESPONSE_EVENTS = 'RESPONSE_EVENTS';
 }
