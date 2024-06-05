bdbd39722cbb24f0fa552ba8731e317268dd2557
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-05 14:37:32 +0400

    added interfaces for environmet

--- src/Cache/CacheItemPoolInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Henrik\Contracts\Cache;
-
-use Psr\Cache\CacheItemPoolInterface as PsrCacheItemPoolInterface;
-
-interface CacheItemPoolInterface extends PsrCacheItemPoolInterface {}
