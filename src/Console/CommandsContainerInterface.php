cd5543d95d6c0f6c2dc0eb5e1e436841287836de
Henrik Karapetyan <g.karapetyan@tool-kit.tech>
2024-06-19 10:57:17 +0400

    added new method getAll into CommnadsContainerInterface

--- src/Console/CommandsContainerInterface.php
+++ src/Console/CommandsContainerInterface.php
@@ -7,4 +7,7 @@ use Psr\Container\ContainerInterface;
 interface CommandsContainerInterface extends ContainerInterface
 {
     public function set(string $id, mixed $value): void;
+
+    /** @return  array<string, CommandDefinitionInterface> */
+    public function getAll(): array;
 }
